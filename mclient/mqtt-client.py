f8ed8426d9f5b270e8853a66815acc5c7b61a713
Monsur Ahmed Chowdhury <62332686+macrifat@users.noreply.github.com>
2022-08-18 11:39:33 +0200

    update comments

--- mclient/mqtt-client.py
+++ mclient/mqtt-client.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt 
 import time
 
-mqttBroker ="192.168.0.100"
+mqttBroker ="192.168.0.100" # Ip address
 
 client = mqtt.Client("First Mqtt Message")
 client.connect(mqttBroker, 11885) 
 
 while True:
-    client.publish("new topic", "My name is rifat")
+    client.publish("new topic", "My name is rifat") # publishing message to the specific topic
     print("Just published My name")
     time.sleep(1)
